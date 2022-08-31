c2fc65e2d46ab9a2590ae76304ef4994fcfb70df
Guilherme Cruz <71985319+guilhermehccruz@users.noreply.github.com>
2022-08-31 00:24:33 -0300

    fix: lava-queue bugs with livestreams (#74)

--- 5-music-player-lavalink/src/commands/queue.ts
+++ 5-music-player-lavalink/src/commands/queue.ts
@@ -154,8 +154,17 @@ export class MusicQueue extends Queue {
 
     const bar = (this.isPlaying ? "▶️" : "⏸️") + " " + progressString;
     const currentTime = this.fromMS(timeNow);
-    const endTime = this.fromMS(timeTotal);
-    const spacing = bar.length - currentTime.length - endTime.length;
+
+    let endTime: string;
+    let spacing: number;
+    if (currentTrack.info.isStream) {
+      endTime = "Livestream";
+      spacing = 8.5;
+    } else {
+      endTime = this.fromMS(timeTotal);
+      spacing = bar.length - currentTime.length - endTime.length;
+    }
+
     const time =
       "`" + currentTime + " ".repeat(spacing * 3 - 2) + endTime + "`";
 
@@ -231,12 +240,17 @@ export class MusicQueue extends Queue {
 
       const queue = this.tracks
         .slice(currentPage * 10, currentPage * 10 + 10)
-        .map(
-          (track, index1) =>
+        .map((track, index1) => {
+          const endTime = track.info.isStream
+            ? "Livestream"
+            : this.fromMS(track.info.length);
+
+          return (
             `${currentPage * 10 + index1 + 1}. [${track.info.title}](<${
               track.info.uri
-            }>)` + ` (${this.fromMS(track.info.length)})`
-        )
+            }>)` + ` (${endTime})`
+          );
+        })
         .join("\n\n");
 
       return { content: `${current}\n\n${queue}` };
